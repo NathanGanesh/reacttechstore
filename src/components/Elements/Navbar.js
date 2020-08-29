f4436b0945c05b7b1f45455d8478e4e5c0166d74
nathanganesh <nathangemieee@gmail.com>
2020-08-29 14:59:07 +0200

    nav bar fix side

--- src/components/Elements/Navbar.js
+++ src/components/Elements/Navbar.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaCartPlus, FaBars } from 'react-icons/fa';
+import SideBar from './SideBar';
 
 export default function Navbar() {
-	console.log(window.location.origin + '/page-images/images/logo.svg');
+	// console.log(window.location.origin + '/page-images/images/logo.svg');
+	const [ index, setIndex ] = useState(0);
+
+	function handeClick(event) {
+		if (index === 0) {
+			setIndex(1);
+		} else {
+			setIndex(0);
+		}
+	}
+
 	return (
 		<nav className="flex-nav">
-			<FaBars className="nav-icon" />
+			<FaBars onClick={(event) => handeClick(event)} className="nav-icon" />
+			<SideBar index={index} />
 			<img src={window.location.origin + '/page-images/images/logo.svg'} alt="logo" />
 			<FaCartPlus className="nav-icon" />
 		</nav>
--- src/components/Elements/SideBar.js
+++ src/components/Elements/SideBar.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function SideBar() {
-	const [ index, setIndex ] = useState(0);
+export default function SideBar({ index }) {
+	console.log(index);
+	function displayPage(index) {
+		let jungle = '';
+		index === 1 ? (jungle = 'inline-block') : (jungle = 'none');
 
-	return (
-		<div className="side-bar">
-			<Link className="sidebar-link" to={'/'}>
-				<div>Home</div>
-			</Link>
-			<Link className="sidebar-link" to={'/about'}>
-				<div>About</div>
-			</Link>
-			<Link className="sidebar-link" to="/products">
-				<div>Products</div>
-			</Link>
-			<Link className="sidebar-link" to="/contact">
-				<div>Contact</div>
-			</Link>
-			<Link className="sidebar-link" to="/cart">
-				<div>Cart</div>
-			</Link>
-		</div>
-	);
+		let zindex = 0;
+
+		index === 1 ? (zindex = 1000) : (zindex = -1000);
+
+		console.log(jungle);
+		switch (index) {
+			default:
+			case 1:
+				return (
+					<div className="side-bar" style={{ zIndex: zindex, display: `${jungle}` }}>
+						<Link className="sidebar-link" to={'/'}>
+							<div>Home</div>
+						</Link>
+						<Link className="sidebar-link" to={'/about'}>
+							<div>About</div>
+						</Link>
+						<Link className="sidebar-link" to="/products">
+							<div>Products</div>
+						</Link>
+						<Link className="sidebar-link" to="/contact">
+							<div>Contact</div>
+						</Link>
+						<Link className="sidebar-link" to="/cart">
+							<div>Cart</div>
+						</Link>
+					</div>
+				);
+		}
+	}
+
+	return displayPage(index);
 }
